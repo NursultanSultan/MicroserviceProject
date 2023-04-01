03eb3ee9c01a9fb943707945f696dcc9bb8cade8
NursultanSultan <tu201906251@code.edu.az>
2023-04-01 11:53:50 +0400

    'feat: I added IEventBusSubscriptionManager and InMemory implementation'

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
@@ -0,0 +1,34 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBusSubscriptionManager
+    {
+        bool IsEmpty { get;}
+
+        event EventHandler<string> OnEventRemoved;
+
+        void AddSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+        void RemoveSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+        bool HasSubscriptionForEvent<T>() where T : IntegrationEvent;
+
+        bool HasSubscriptionForEvent(string eventName);
+
+        Type GetEventTypeByName(string eventName);
+
+        void Clear();
+
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent;
+
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName);
+
+        string GetEventKey<T>();
+    }
+}
